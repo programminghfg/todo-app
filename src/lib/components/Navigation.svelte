e536fe3d1f3e8e33864d076f87d3b562e13396ca
Radialarray <25952866+Radialarray@users.noreply.github.com>
2022-10-18 14:46:54 +0200

    add navigation

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<nav>
+	<ul>
+		<li><a href="/" class:active={$page.url.pathname === '/'}>Home</a></li>
+		<li><a href="/todo" class:active={$page.url.pathname === '/todo'}>Todo</a></li>
+	</ul>
+</nav>
+
+<style>
+	a {
+		text-decoration: none;
+		color: black;
+	}
+
+	ul {
+		list-style: none;
+		display: flex;
+		gap: 8px;
+	}
+
+	.active {
+		border-bottom: 2px solid black;
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Navigation from '$lib/components/Navigation.svelte';
+	import '$lib/styles/main.css';
+</script>
+
+<Navigation />
+
+<main class="grid container">
+	<slot />
+</main>
